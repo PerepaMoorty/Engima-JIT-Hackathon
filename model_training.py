5d65b980595df5fa087924b600ef50af7640ae10
AmeetZ <ameetzdev@gmail.com>
2024-12-13 22:14:33 +0530

    Testing ML Models

--- model_training.py
+++ model_training.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+import sklearn.ensemble
 
 
 def train_model(data):
@@ -11,7 +11,7 @@ def train_model(data):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    model = RandomForestClassifier()
+    model = sklearn.ensemble.AdaBoostClassifier
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
     data["prediction"] = model.predict(X[features])
