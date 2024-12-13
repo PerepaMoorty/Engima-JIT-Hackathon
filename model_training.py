fa8e02f037527e8d4452c3da0db3c1b7c2369bc1
AmeetZ <ameetzdev@gmail.com>
2024-12-13 22:44:00 +0530

    Testing ML Models

--- model_training.py
+++ model_training.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import train_test_split
 import sklearn.ensemble
 
 
-def train_model(data):
+def train_model(data, n_estimators=100):
     data["target"] = (data["close"].shift(-1) > data["close"]).astype(int)
     features = ["RSI", "MACD", "Signal_Line", "BB_High", "BB_Low"]
     data = data.dropna()
@@ -11,7 +11,7 @@ def train_model(data):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    model = sklearn.ensemble.GradientBoostingClassifier()
+    model = sklearn.ensemble.GradientBoostingClassifier(n_estimators=n_estimators)
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
     data["prediction"] = model.predict(X[features])
